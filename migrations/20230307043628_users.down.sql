0f0ae3ef9d1f4fc5fbf04748868eb7a29e317d51
enforcer007 <akhilgodvsdemon@gmail.com>
2023-03-07 06:41:40 +0000

    db setup

---
+++ migrations/20230307043628_users.down.sql
@@ -0,0 +1,7 @@
+-- Add down migration script here
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS data;
+DROP TABLE IF EXISTS instagram;
+DROP TABLE IF EXISTS facebook;
+DROP TABLE IF EXISTS github;
+DROP TABLE IF EXISTS contact;
